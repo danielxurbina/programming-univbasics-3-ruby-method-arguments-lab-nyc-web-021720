46ea1e6ce3ae80fd80336c3ba09aedba31bb7cea
Daniel Urbina <danielur1999@gmail.com>
2020-02-03 15:35:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,8 +7,13 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and i write #{language}!"
+end
+
 introduction("Daniel")
 
+introduction_with_language_optional("Daniel")
 
 
 
